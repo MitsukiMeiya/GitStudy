dd03c7e9db3a2c207f7a60854612f1e551eefc97
MitsukiMeiya <cldus26@gmail.com>
2025-03-20 12:44:19 +0900

    Add Function
    새로운 기능 추가

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -4,10 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("이름을 임력해 주세요 : ");
-            string input = Console.ReadLine();
-
-            Console.WriteLine("입력하신 이름은 {0} 입니다.", input);
+            Console.WriteLine("새로운 기능 추가!");
 
         }
     }
